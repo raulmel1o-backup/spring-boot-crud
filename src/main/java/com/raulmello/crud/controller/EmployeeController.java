a5156127b3de473e4595029108d09001a9fa3f03
Raul Mello <raulmello@usp.br>
2021-05-07 19:22:39 -0300

    Creation of a business logic layer inside Services directory

--- src/main/java/com/raulmello/crud/controller/EmployeeController.java
+++ src/main/java/com/raulmello/crud/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.raulmello.crud.controller;
 
 import com.raulmello.crud.model.Employee;
 import com.raulmello.crud.repository.EmployeeRepository;
+import com.raulmello.crud.services.EmployeeServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,50 +11,34 @@ import java.util.List;
 @RestController
 public class EmployeeController {
 
-    private final EmployeeRepository repository;
+    private final EmployeeServices employeeServices;
 
-    EmployeeController(EmployeeRepository employeeRepository) {
-        this.repository = employeeRepository;
+    EmployeeController(EmployeeRepository employeeRepository, EmployeeServices employeeServices) {
+        this.employeeServices = employeeServices;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return repository.findAll();
+        return employeeServices.findAll();
     }
 
     @GetMapping("/employee/{id}")
     public ResponseEntity findById(@PathVariable long id) {
-        return repository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
+        return employeeServices.findById(id);
     }
 
     @PostMapping("/employee")
     public Employee create(@RequestBody Employee employee) {
-        return repository.save(employee);
+        return employeeServices.create(employee);
     }
 
     @PutMapping("/employee/{id}")
     public ResponseEntity update(@PathVariable("id") long id, @RequestBody Employee employee) {
-        return repository.findById(id).map(record -> {
-            record.setFirstName(employee.getFirstName());
-            record.setLastName(employee.getLastName());
-            record.setInscriptionId(employee.getInscriptionId());
-            record.setPosition(employee.getPosition());
-            record.setBranch(employee.getBranch());
-            record.setManager(employee.getManager());
-            record.setIncome(employee.getIncome());
-
-            final Employee updated = repository.save(record);
-            return ResponseEntity.ok().body(updated);
-
-        }).orElse(ResponseEntity.notFound().build());
+        return employeeServices.update(id, employee);
     }
 
     @DeleteMapping("/employee/{id}")
     public ResponseEntity delete(@PathVariable long id) {
-        return repository.findById(id).map(record -> {
-            repository.deleteById(id);
-
-            return ResponseEntity.ok().body(record);
-        }).orElse(ResponseEntity.notFound().build());
+        return employeeServices.delete(id);
     }
 }
